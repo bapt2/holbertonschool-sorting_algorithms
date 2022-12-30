29136ed7e6a77d27155d48446d1f16588e1137e0
bapt2 <5711@holbertonstudents.com>
2022-12-30 06:00:56 -0800

    fixed betty style and added null condition

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,14 +1,24 @@
 #include "sort.h"
 
+/**
+ * bubble_sort - function that sorts an array of integers in ascending
+ * order using the Bubble sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ * Return: void
+*/
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, count, tmp;
+	size_t i, tmp, count;
 
+	if (!array || size < 2)
+		return;
 	for (count = 0; count < size - 1; ++count)
 	{
 		for (i = 0; i < size - count - 1; ++i)
 		{
-			if (array[i] > array[i + 1])
+			if (array[i + 1] != '\0' && array[i] > array[i + 1])
 			{
 				tmp = array[i];
 				array[i] = array[i + 1];
